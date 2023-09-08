7fac02efd3b6e74d8ecb83e2cbabaf5375b75aac
Garrett Leber <lebergarrett@gmail.com>
2023-09-07 20:42:16 -0400

    slight improvements

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import sys
 import os
 
 from flask import Flask, request, jsonify
+from dotenv import load_dotenv
 
-SERVERS_DIR = "/srv"
+load_dotenv()
+
+SERVERS_DIR = str(os.getenv('MC_SERVERS_DIR'))
 if not os.path.isdir(SERVERS_DIR):
     sys.exit("SERVERS_DIR doesn't exist or is not a directory")
-
 
 app = Flask(__name__)
 
@@ -37,7 +39,6 @@ def start():
 def stop():
     server = request.json.get('server')
     try:
-        # Run the 'docker-compose down' command to stop the stack
         subprocess.run(['docker-compose', '-f', f'{SERVERS_DIR}/{server}/docker-compose.yml', 'down', '-v', '--remove-orphans'], check=True)
         return jsonify({'message': f'Stopped stack {server}'})
     except subprocess.CalledProcessError as e:
@@ -56,10 +57,10 @@ def read_existing_time(file_path):
         print("No timefile to read")
     return None
 
-# Function to update the time in the file if the new time is greater
 def update_timefile(file_path, new_time):
     existing_time = read_existing_time(file_path)
 
+    # Only update time if it is greater than what is present in the file
     if existing_time is None or new_time > existing_time:
         directory_path = os.path.dirname(file_path)
         os.makedirs(directory_path, exist_ok=True)
@@ -74,7 +75,6 @@ def extendtime():
     seconds = int(days) * 86400
     timefile = f'/data/{server}/timefile'
     server_dir = f'{SERVERS_DIR}/{server}/data'
-    skipfile = '.skip-stop'
 
     try:
         update_timefile(timefile, int(time.time()) + seconds)
@@ -82,9 +82,9 @@ def extendtime():
         dirs = [d for d in os.listdir(server_dir) if os.path.isdir(os.path.join(server_dir,d))]
         for dir in dirs:
             # https://docker-minecraft-server.readthedocs.io/en/latest/misc/autopause-autostop/autostop/
-            file = os.path.join(server_dir, dir, skipfile)
-            print(f"Adding skipfile {file}")
-            with open(file, "w"):
+            skipfile = os.path.join(server_dir, dir, '.skipfile')
+            print(f"Adding skipfile {skipfile}")
+            with open(skipfile, "w"):
                 pass
 
         return jsonify({'message': f'Extended time for {server} another {days} days'})
