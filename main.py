62b52bf0b82525f066b157664c7b0264c2cd1313
Garrett Leber <garrett@leberra.com>
2025-07-05 15:17:26 -0400

    formatting, update autostop time

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-import subprocess
 import logging
-import time
-import yaml
-import sys
 import os
+import subprocess
+import sys
+import time
 
-from flask import Flask, request, jsonify
+import yaml
 from dotenv import load_dotenv
+from flask import Flask, jsonify, request
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
-SERVERS_DIR = str(os.getenv('MC_SERVERS_DIR'))
+SERVERS_DIR = str(os.getenv("MC_SERVERS_DIR"))
 if not os.path.isdir(SERVERS_DIR):
     sys.exit(f"SERVERS_DIR: ({SERVERS_DIR}) doesn't exist or is not a directory")
+
 
 def get_mcbot_data(server: str) -> dict:
     """
     Gets data out of a specified yaml file which describes information about a server
 
     $server is a relative path to a directory in $SERVERS_DIR
-
-    TESTCHANGE
     """
     mcbot_data_file = os.path.join(SERVERS_DIR, server, "mcbot.yaml")
     logging.info(f"Checking for mcbot config file ({mcbot_data_file})")
@@ -33,17 +32,20 @@ def get_mcbot_data(server: str) -> dict:
         logging.debug(data)
         return data
     else:
-        logging.warn("Did not find mcbot config file")
+        logging.warning("Did not find mcbot config file")
         return {}
 
 
 app = Flask(__name__)
 
-@app.route('/list', methods=['GET'])
+
+@app.route("/list", methods=["GET"])
 def list():
     try:
         items = os.listdir(SERVERS_DIR)
-        directories = [item for item in items if os.path.isdir(os.path.join(SERVERS_DIR, item))]
+        directories = [
+            item for item in items if os.path.isdir(os.path.join(SERVERS_DIR, item))
+        ]
 
         servers = []
         for server in directories:
@@ -55,41 +57,61 @@ def list():
 
             servers.append(server_info)
 
-        return jsonify({'message': servers})
+        return jsonify({"message": servers})
     except Exception:
-        return jsonify({'error': 'Error listing servers'}), 500
+        return jsonify({"error": "Error listing servers"}), 500
 
-@app.route('/start', methods=['POST'])
+
+@app.route("/start", methods=["POST"])
 def start():
-    server = request.json.get('server') # type: ignore
+    server = request.json.get("server")  # type: ignore
     data = get_mcbot_data(server)
 
     try:
         # Run the 'docker-compose up' command to start the stack
-        subprocess.Popen(['docker-compose', '-f', f'{SERVERS_DIR}/{server}/docker-compose.yml', 'up', '-d'])
+        subprocess.Popen(
+            [
+                "docker-compose",
+                "-f",
+                f"{SERVERS_DIR}/{server}/docker-compose.yml",
+                "up",
+                "-d",
+            ]
+        )
         message = f"""
-        Started {server},
-        You can access it at {data["hostname"]}
+Started {server},
+You can access it at {data["hostname"]}
 
-        Please allow time for the server to start. You have 30 minutes to connect or the server will automatically shut down.
-        If you disconnect, it will automatically shut down after 30 minutes.
+Please allow time for the server to start.
+If you disconnect, it will automatically shut down after 36 hours.
         """
-        return jsonify({'message': message.strip()})
+        return jsonify({"message": message.strip()})
     except subprocess.CalledProcessError as e:
-        return jsonify({'error': e.stderr.decode()}), 500
+        return jsonify({"error": e.stderr.decode()}), 500
 
-@app.route('/stop', methods=['POST'])
+
+@app.route("/stop", methods=["POST"])
 def stop():
-    server = request.json.get('server')  # type: ignore
+    server = request.json.get("server")  # type: ignore
     try:
-        subprocess.Popen(['docker-compose', '-f', f'{SERVERS_DIR}/{server}/docker-compose.yml', 'down', '-v', '--remove-orphans'])
-        return jsonify({'message': f'Stopped {server}'})
+        subprocess.Popen(
+            [
+                "docker-compose",
+                "-f",
+                f"{SERVERS_DIR}/{server}/docker-compose.yml",
+                "down",
+                "-v",
+                "--remove-orphans",
+            ]
+        )
+        return jsonify({"message": f"Stopped {server}"})
     except subprocess.CalledProcessError as e:
-        return jsonify({'error': e.stderr.decode()}), 500
+        return jsonify({"error": e.stderr.decode()}), 500
+
 
 def read_existing_time(file_path):
     if os.path.exists(file_path):
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             try:
                 time = int(file.read().strip())
                 logging.debug(f"Read timefile, time: {time}")
@@ -100,6 +122,7 @@ def read_existing_time(file_path):
         logging.debug("No timefile to read")
     return None
 
+
 def update_timefile(file_path, new_time):
     existing_time = read_existing_time(file_path)
 
@@ -107,38 +130,42 @@ def update_timefile(file_path, new_time):
     if existing_time is None or new_time > existing_time:
         directory_path = os.path.dirname(file_path)
         os.makedirs(directory_path, exist_ok=True)
-        with open(file_path, 'w') as file:
+        with open(file_path, "w") as file:
             file.write(str(new_time))
             logging.info(f"Updated time in {file_path} to {new_time}")
 
-@app.route('/extendtime', methods=['POST'])
+
+@app.route("/extendtime", methods=["POST"])
 def extendtime():
-    server = request.json.get('server')  # type: ignore
-    days = request.json.get('days')  # type: ignore
+    server = request.json.get("server")  # type: ignore
+    days = request.json.get("days")  # type: ignore
     seconds = int(days) * 86400
-    timefile = f'/data/{server}/timefile'
-    server_dir = f'{SERVERS_DIR}/{server}/data'
+    timefile = f"/data/{server}/timefile"
+    server_dir = f"{SERVERS_DIR}/{server}/data"
 
     try:
         update_timefile(timefile, int(time.time()) + seconds)
 
-        dirs = [d for d in os.listdir(server_dir) if os.path.isdir(os.path.join(server_dir,d))]
+        dirs = [
+            d
+            for d in os.listdir(server_dir)
+            if os.path.isdir(os.path.join(server_dir, d))
+        ]
         for dir in dirs:
             # https://docker-minecraft-server.readthedocs.io/en/latest/misc/autopause-autostop/autostop/
-            skipfile = os.path.join(server_dir, dir, '.skip-stop')
+            skipfile = os.path.join(server_dir, dir, ".skip-stop")
             logging.info(f"Adding skipfile {skipfile}")
             with open(skipfile, "w"):
                 pass
 
         message = f"""
-        Extended time for {server} another {days} days.
-        The server will remain online for this entire time even with nobody online.
+Extended time for {server} another {days} days.
+The server will remain online for this entire time even with nobody online.
         """
-        return jsonify({'message': message.strip()})
+        return jsonify({"message": message.strip()})
     except subprocess.CalledProcessError as e:
-        return jsonify({'error': e.stderr.decode()}), 500
+        return jsonify({"error": e.stderr.decode()}), 500
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
